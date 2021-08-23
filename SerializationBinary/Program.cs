043bd36c60977f0b57e5af99cdcf60861de3bb7b
Sagar Lad <sagar.lad@morningstar.com>
2021-08-23 16:19:08 +0530

    Added Xml Serialization

--- SerializationBinary/Program.cs
+++ SerializationBinary/Program.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
 
 namespace SerializationBinary
 {
@@ -8,60 +13,80 @@ namespace SerializationBinary
     {
         static void Main(string[] args)
         {
-            Person person = new Person() { FirstName = "Sagar", LastName = "lad" };
+            Person person = new Person() { FirstName = "Sagar ", LastName = "Lad" };
             string filePath = "data.save";
-
             DataSerializer dataSerializer = new DataSerializer();
             Person p = null;
 
-            dataSerializer.BinarySerialize(person, filePath);
-            p = dataSerializer.BinaryDeserialize(filePath) as Person;
+            //dataSerializer.BinarySerialze(person, filePath);
+            //p = dataSerializer.BinaryDeserialize(filePath) as Person;
 
-            Console.WriteLine(p.FirstName);
+            dataSerializer.XmlSerialize(typeof(Person), person, filePath);
+            p = dataSerializer.XmlDeserialize(typeof(Person), filePath) as Person;
+
+            Console.WriteLine(p.FirstName); ;
             Console.WriteLine(p.LastName);
+        }
+    }
+    [Serializable]
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class DataSerializer
+    {
+        public void BinarySerialze(object data, string filePath)
+        {
+            FileStream fileStream;
+            BinaryFormatter bf = new BinaryFormatter();
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+            fileStream = File.Create(filePath);
+            bf.Serialize(fileStream, data);
+            fileStream.Close();
 
         }
-
-        [Serializable]
-        public class Person
+        public object BinaryDeserialize(Type dataType, string filePath)
         {
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-        }
+            object obj = null;
 
-        class DataSerializer
-        {
-            public void BinarySerialize(object data, string filePath)
+            FileStream fileStream;
+            BinaryFormatter bf = new BinaryFormatter();
+            if (File.Exists(filePath))
             {
-                FileStream fileStream;
-                BinaryFormatter bf = new BinaryFormatter();
-
-                if (File.Exists(filePath))
-                    File.Delete(filePath);
-
-                fileStream = File.Create(filePath);
-                bf.Serialize(fileStream, data);
-
+                fileStream = File.OpenRead(filePath);
+                obj = bf.Deserialize(fileStream);
                 fileStream.Close();
             }
-
-
-            public object BinaryDeserialize(string filePath)
+            return obj;
+        }
+        public void XmlSerialize(Type dataType, object data, string filePath)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(dataType);
+            if (File.Exists(filePath))
             {
-                object obj = null;
-
-                FileStream fileStream;
-                BinaryFormatter bf = new BinaryFormatter();
-                if (File.Exists(filePath))
-                {
-                    fileStream = File.OpenRead(filePath);
-                    obj = bf.Deserialize(fileStream);
-                    fileStream.Close();
-                }
-
-                return obj;
+                File.Delete(filePath);
             }
+            TextWriter writer = new StreamWriter(filePath);
+            xmlSerializer.Serialize(writer, data);
+            writer.Close();
 
         }
+        public object XmlDeserialize(Type dataType, string filePath)
+        {
+            object obj = null;
+
+            XmlSerializer xmlSerializer = new XmlSerializer(dataType);
+            if (File.Exists(filePath))
+            {
+                TextReader textReader = new StreamReader(filePath);
+                obj = xmlSerializer.Deserialize(textReader);
+                textReader.Close();
+            }
+            return obj;
+        }
+
     }
 }
